aed955db5c1faea0d9fdee955dcdc4b9747e7e65
Tancredo Souza <aqueletantan@gmail.com>
2020-06-12 14:41:44 -0300

    feat: add some material-ui components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  paperPadding: {
+    padding: theme.spacing(4, 4),
+  },
+  title: {
+    fontFamily: "Baloo Tamma 2",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  bodyText: {
+    fontSize: 18,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div className={classes.title}>tancredo</div>
+        <p className={classes.bodyText}>
+          Changing the world, one line of clean code at a time.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Paper elevation={3} className={classes.paperPadding}>
+          <div className={classes.title}>Projects I'm prowdest of</div>
+        </Paper>
       </header>
     </div>
   );
-}
+};
 
 export default App;
