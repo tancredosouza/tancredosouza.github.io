02f0c72040f86bbbd5ac1191bd336268a6595615
Tancredo Souza <aqueletantan@gmail.com>
2020-06-17 17:27:10 -0300

    Updates

---
+++ precache-manifest.00787184b7a1954919fd6e93b1a5c4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a3e044e878f155826acddec26a4f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998cb5139ff3f7e19ef7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a7f7a5639c4b3620fd2",
+    "url": "/static/js/2.0abb1113.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0abb1113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998cb5139ff3f7e19ef7",
+    "url": "/static/js/main.3b48de2e.chunk.js"
+  },
+  {
+    "revision": "3db20d1a8fd9b2f869f4",
+    "url": "/static/js/runtime-main.48ab15a8.js"
+  },
+  {
+    "revision": "c3abba50393aeb36cfb53b345546fd2a",
+    "url": "/static/media/rainbow-bg.c3abba50.png"
+  }
+]);
